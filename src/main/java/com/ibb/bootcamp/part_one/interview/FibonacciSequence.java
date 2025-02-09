0c9ecbf34a20596d143fe04631b6ed08b2d770f8
Son Kod Bükücü <basrioglumehmet@gmail.com>
2025-02-09 14:14:07 +0300

    Interview Solutions

---
+++ src/main/java/com/ibb/bootcamp/part_one/interview/FibonacciSequence.java
@@ -0,0 +1,21 @@
+package com.ibb.bootcamp.part_one.interview;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        // The Fibonacci sequence is the series of numbers where each number is the sum of the two preceding numbers.
+        // For example, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610
+        // Big O Notation = O(n) FASTER
+        int number1 = 0;
+        int number2 = 1;
+        for (int i = 0; i < 90; i++) {
+            //Print the first replaced number (it's used to print the sequence).
+            System.out.print(String.format("%d,",number1));
+            int result = number2 + number1;
+            //Swapping Operation
+            //Example: 0(number1),1(number2) replaced to 1(number2) + 1(sum)
+            number1 = number2;
+            number2 = result;
+        }
+        System.out.println("");
+    }
+}
---
+++ src/main/java/com/ibb/bootcamp/part_one/interview/PrimeNumber.java
@@ -0,0 +1,28 @@
+package com.ibb.bootcamp.part_one.interview;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        //Check the user input is a valid prime number.
+        /*
+            a number that can only be divided by itself and 1 without remainders
+            WARNING! 1 only has one factor, so 1 isn't a prime number.
+        */
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        boolean isPrime = true;
+        if (num == 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if ((num % i) == 0) {
+                    isPrime = false;
+                    break; // As soon as a divisor is found, we can break out of the loop.
+                }
+            }
+        }
+        System.out.println(String.format("Is Prime Number: %s (%d)",isPrime == true ? "Yes":"No",num));
+
+    }
+}
