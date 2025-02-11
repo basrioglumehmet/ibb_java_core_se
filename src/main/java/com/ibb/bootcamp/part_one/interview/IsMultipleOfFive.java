59a46b242a1920dd6a09be9c80cf383df7f2f3f0
Son Kod Bükücü <basrioglumehmet@gmail.com>
2025-02-11 18:25:11 +0300

    Interview Solutions

---
+++ src/main/java/com/ibb/bootcamp/part_one/interview/IsMultipleOfFive.java
@@ -0,0 +1,18 @@
+package com.ibb.bootcamp.part_one.interview;
+
+import java.util.Scanner;
+
+public class IsMultipleOfFive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write a number:");
+        int number = scanner.nextInt();
+
+        if(number % 5 == 0){
+            System.out.println("Multiple of 5");
+        }
+        else{
+            System.out.println("Not a multiple of 5.");
+        }
+    }
+}
---
+++ src/main/java/com/ibb/bootcamp/part_one/interview/SumOfAllNumbers.java
@@ -0,0 +1,15 @@
+package com.ibb.bootcamp.part_one.interview;
+
+import java.util.Scanner;
+
+public class SumOfAllNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write a number:");
+        int number = scanner.nextInt(), sum = 0;
+        for (int i = 0; i < number; i++) {
+            sum += i;
+        }
+        System.out.println(String.format("Sum result:%d",sum));
+    }
+}
